14e059de7907daeda30808531d4a75a316e1642d
ignaciomartind8 <ignacio8martin@gmail.com>
2023-10-22 17:32:55 -0300

    Agregada opcion de selccion GUI - CLI

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,11 +1,31 @@
 from CLI import CLI
+from GUI import MainWindow
+from PyQt5.QtWidgets import QApplication
+import sys
 
 
 def main():
 
-    cli = CLI()
-    cli.prompt = '--> '
-    cli.cmdloop('Iniciando entrada de comandos. Usar help para ver comandos.')
+    UI = str.upper(input("Enter 'CLI' for CLI or 'GUI' for GUI: "))
+
+    if UI == "CLI":
+
+        print("Corriendo CLI...")
+        cli = CLI()
+        cli.prompt = '--> '
+        cli.cmdloop('Iniciando entrada de comandos. Usar help para ver comandos.')
+
+    elif UI == "GUI":
+
+        print("Corriendo GUI...")
+        app = QApplication(sys.argv)
+        mainWindow = MainWindow()
+        mainWindow.show()
+        sys.exit(app.exec_())
+
+    else:
+        print("Opcion no valida.")
+
 
 
 if __name__ == "__main__":
